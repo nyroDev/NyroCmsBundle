ee5b719842aee96d0019b0f93bc5f8c2b1bc4c76
nyro <cedric@nyrodev.com>
2016-01-07 10:38:12 +0100

    Fix ContentHandlerFilterType action

--- Form/Type/ContentHandlerFilterType.php
+++ Form/Type/ContentHandlerFilterType.php
@@ -8,7 +8,7 @@ class ContentHandlerFilterType extends Type\AbstractFilterType {
 
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-			->setAction($this->generateUrl('nyrocms_admin_data_user'))
+			->setAction($this->generateUrl('nyrocms_admin_data_contentHandler'))
 			->add('id', Type\FilterIntType::class, array('label'=>$this->trans('admin.contentHandler.id')))
 			->add('name', Type\FilterType::class, array('label'=>$this->trans('admin.contentHandler.name')))
 			->add('class', Type\FilterType::class, array('label'=>$this->trans('admin.contentHandler.class')))
