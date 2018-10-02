87cc7f6190cf8fb361a6f1993a9b409650658716
nyro <cedric@nyrodev.com>
2018-10-02 07:25:50 +0200

    Update cssTemplate to use all as media

--- Resources/views/Composer/cssTemplate.html.php
+++ Resources/views/Composer/cssTemplate.html.php
@@ -2,7 +2,7 @@
     '@css_all',
     array('?yui_css'),
     array('output' => 'css/all.css')) as $url): ?>
-	<link rel="stylesheet" type="text/css" media="screen" href="<?php echo $view->escape($view['nyrodev']->getAsseticVersionUrl($url)) ?>" />
+	<link rel="stylesheet" type="text/css" media="all" href="<?php echo $view->escape($view['nyrodev']->getAsseticVersionUrl($url)); ?>" />
 <?php $cssAll[] = $view['nyrodev']->getAsseticVersionUrl($url); endforeach; ?>
 
 <?php
@@ -12,33 +12,29 @@ foreach ($view['assetic']->stylesheets(
     array('?yui_css'),
     array('output' => 'css/tablet.css')) as $url) {
     $url = $view->escape($view['nyrodev']->getAsseticVersionUrl($url));
-    $ieCss[] = $url;
-    ?>
-	<link rel="stylesheet" type="text/css" media="all and (min-width: <?php echo $tabletWidth ?>)" href="<?php echo $url ?>" />
+    $ieCss[] = $url; ?>
+	<link rel="stylesheet" type="text/css" media="all and (min-width: <?php echo $tabletWidth; ?>)" href="<?php echo $url; ?>" />
 	<?php
-
 }
 foreach ($view['assetic']->stylesheets(
     '@css_desktop',
     array('?yui_css'),
     array('output' => 'css/desktop.css')) as $url) {
     $url = $view->escape($view['nyrodev']->getAsseticVersionUrl($url));
-    $ieCss[] = $url;
-    ?>
-	<link rel="stylesheet" type="text/css" media="all and (min-width: <?php echo $desktopWidth ?>)" href="<?php echo $url ?>" />
+    $ieCss[] = $url; ?>
+	<link rel="stylesheet" type="text/css" media="all and (min-width: <?php echo $desktopWidth; ?>)" href="<?php echo $url; ?>" />
 	<?php
-
 }
 ?>
 <!--[if lt IE 9]>
 	<?php foreach ($ieCss as $css): ?>
-		<link rel="stylesheet" type="text/css" href="<?php echo $css ?>" />
+		<link rel="stylesheet" type="text/css" href="<?php echo $css; ?>" />
 	<?php endforeach; ?>
 	<?php foreach ($view['assetic']->stylesheets(
         '@css_ie',
         array('?yui_css'),
         array('output' => 'css/ie.css')) as $url): ?>
-		<link rel="stylesheet" type="text/css" media="screen" href="<?php echo $view->escape($view['nyrodev']->getAsseticVersionUrl($url)) ?>" />
+		<link rel="stylesheet" type="text/css" media="all" href="<?php echo $view->escape($view['nyrodev']->getAsseticVersionUrl($url)); ?>" />
 	<?php endforeach; ?>
 	<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<script></script>
