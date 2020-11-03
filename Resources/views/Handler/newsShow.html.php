831ea986c074591b073d636e2dcd21e333f69b07
nyro <cedric@nyrodev.com>
2020-11-03 22:33:06 +0100

    Show intro for news content when there is no composer contents

--- Resources/views/Handler/newsShow.html.php
+++ Resources/views/Handler/newsShow.html.php
@@ -1,4 +1,8 @@
 <?php if (isset($backUrl) && $backUrl): ?>
 	<div class="newsBack"><a href="<?php echo $backUrl; ?>"><?php echo $view['nyrodev']->trans('nyrocms.handler.news.back'); ?></a></div>
 <?php endif; ?>
-<?php echo $view['nyrocms_composer']->render($news); ?>
+<?php if (count($news->getContent())): ?>
+	<?php echo $view['nyrocms_composer']->render($news); ?>
+<?php else: ?>
+	<p><?php echo nl2br($news->getIntro()); ?></p>
+<?php endif; ?>
