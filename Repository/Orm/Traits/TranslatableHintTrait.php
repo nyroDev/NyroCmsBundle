2cd503e01a28d5515880b9a84558956ecd889f9b
nyro <cedric@nyro.dev>
2025-02-10 23:50:08 +0100

    Add use for TranslatableHintTrait

--- Repository/Orm/Traits/TranslatableHintTrait.php
+++ Repository/Orm/Traits/TranslatableHintTrait.php
@@ -2,18 +2,22 @@
 
 namespace NyroDev\NyroCmsBundle\Repository\Orm\Traits;
 
+use Doctrine\ORM\Query;
+use Gedmo\Translatable\Query\TreeWalker\TranslationWalker;
+use Gedmo\Translatable\TranslatableListener;
+
 trait TranslatableHintTrait
 {
     public function setHint($query)
     {
         if (defined('NYRO_LOCALE')) {
-            $query->setHint(\Gedmo\Translatable\TranslatableListener::HINT_TRANSLATABLE_LOCALE, NYRO_LOCALE);
+            $query->setHint(TranslatableListener::HINT_TRANSLATABLE_LOCALE, NYRO_LOCALE);
             $query->setHint(
                 \Gedmo\Translatable\TranslatableListener::HINT_FALLBACK,
                 1 // fallback to default values in case if record is not translated
             );
         }
 
-        $query->setHint(\Doctrine\ORM\Query::HINT_CUSTOM_OUTPUT_WALKER, 'Gedmo\\Translatable\\Query\\TreeWalker\\TranslationWalker');
+        $query->setHint(Query::HINT_CUSTOM_OUTPUT_WALKER, TranslationWalker::class);
     }
 }
